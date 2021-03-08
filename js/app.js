4530d77ab3606b20bcb05b24ec9c411405c29b37
Erik Mortensen <mortensenerikm@gmail.com>
2021-03-07 21:58:42 -0300

    Mensajes de error

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 
+
+
 // Constructores
 function seguro(marca, year, tipo){
     this.marca = marca;
@@ -23,6 +25,28 @@ UI.prototype.llenarOpciones = () => {
     }
 }
 
+// Muestra alertas en pantalla
+UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+    const div = document.createElement('div');
+
+    div.classList.add('mensaje', 'mt-10');
+
+    if(tipo === 'error'){
+        div.classList.add('error');
+    } else {
+        div.classList.add('correcto');
+    }
+
+    div.textContent = mensaje;
+
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout(() => {
+        div.remove();
+    }, 1000);
+}
+
 
 // Instanciar UI
 const ui = new UI();
@@ -31,3 +55,30 @@ const ui = new UI();
 document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones();
 })
+
+
+eventListeners();
+function eventListeners(){
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e){
+    e.preventDefault();
+
+    // Leer la marca seleccionada
+    const marca = document.querySelector('#marca').value;
+
+    // Leer el year seleccionado
+    const year = document.querySelector('#year').value;
+
+    // Leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+
+    if(marca === ''  ||  year === ''  ||  tipo === ''){
+        ui.mostrarMensaje('Todos los campos son obligatorios.','error');
+    } else {
+
+    }
+}
