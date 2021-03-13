dfb4ecfb74b7246dcf4deec97c124e9c8afe9b83
Erik Mortensen <mortensenerikm@gmail.com>
2021-03-13 20:03:50 -0300

    Muestra total a pagar

--- js/app.js
+++ js/app.js
@@ -47,8 +47,7 @@ Seguro.prototype.cotizarSeguro = function() {
    } else{
        cantidad *= 1.5;
    }
-
-    console.log(cantidad);
+    return cantidad;
 };
 
 function UI() {}
@@ -87,8 +86,50 @@ UI.prototype.mostrarMensaje = (mensaje, tipo) => {
 
     setTimeout(() => {
         div.remove();
-    }, 1000);
+    }, 1500);
 }
+
+UI.prototype.mostrarResultado = (total, seguro) => {
+
+    const {marca, year, tipo} = seguro;
+    let textoMarca;
+
+    switch (marca) {
+        case '1':
+            textoMarca = 'Americano';
+            break;
+        case '2':
+            textoMarca = 'Asiatico';
+            break;
+        case '3':
+            textoMarca = 'Europeo';
+            break;
+        default:
+            break;
+    }
+
+    const div = document.createElement('div');
+    div.classList.add('mt-10');
+
+    div.innerHTML = `
+        <p class="header">Tu Resumen</p>
+        <p class="font-bold">Marca:  <span class="font-normal"> ${textoMarca} </span></p>
+        <p class="font-bold">Año:  <span class="font-normal"> ${year} </span></p>
+        <p class="font-bold">Tipo:  <span class="font-normal capitalize"> ${tipo} </span></p>
+        <p class="font-bold">Total:  <span class="font-normal">$ ${total} </span></p>
+    `;
+
+    const resultadoDiv = document.querySelector('#resultado');
+
+    // Mostrar el spinner
+    const spinner = document.querySelector('#cargando');
+    spinner.style.display = 'block';
+
+    setTimeout(() => {
+        spinner.style.display = 'none';
+        resultadoDiv.appendChild(div);
+    }, 1500);
+};
 
 
 // Instanciar UI
@@ -125,13 +166,20 @@ function cotizarSeguro(e){
     }
 
     ui.mostrarMensaje('Cotizando...', 'correcto');
+    
+    //  Eliminar cotizaciones previas
+    const resultados = document.querySelector('#resultado div');
+    if(resultados != null){
+        resultados.remove();
+    }
 
     // Instanciar el seguro
     const seguro = new Seguro(marca, year, tipo);
-    seguro.cotizarSeguro();
+    const total = seguro.cotizarSeguro();
 
     // Utilizar el prototype que va a cotizar
-
+    ui.mostrarResultado(total, seguro);
 
 }
+
 
